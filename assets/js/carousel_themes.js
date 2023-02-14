9ea6938e76c9e8599df6e0d0a3e5eeffd633ac0e
sam-omar322 <abdullah.omar@lobb.sa>
2023-02-14 22:23:03 +0300

    change some style

--- assets/js/carousel_themes.js
+++ assets/js/carousel_themes.js
@@ -1,27 +1,56 @@
+var lang = document.getElementsByTagName("html")[0].getAttribute("lang");
+var themes_section_title = document.querySelectorAll(".themes .section-desc")[0];
+var themes_section_hent = document.querySelectorAll(".themes .section-hent")[0];
+
+if (lang == "ar") {
+    var themes_hent = [
+        "ثيم مخصص",
+        "ثيم مخصص",
+        "إشراق"
+    ];
+    var themes_desc = [
+        "تميز متجر قرمز بثيمه الأنيق وأبرز منتجاته بلمسته الخاصة",
+        "في عالم العسل لافييت من ابرز العلامات، ومتجره مخصص على ذوقه الخاص من الصفر",
+        "متجر بالقلم يميز اقلام عملائه ببصمته الخاصة"
+    ];
+} else if (lang == "En") {
+    var themes_hent = [
+        "Elegant Theme",
+        "Custom Theme",
+        "Shine"
+    ];
+    var themes_desc = [
+        "Qormuz has distinguished by its elegant theme and highlighted its products with its own unique touch",
+        "In the world of honey, Lafayette is one of the most prominent brands, and his shop is dedicated to his own style from scratch",
+        "A bilqalam a store that distinguishes the pens of its customers with its own mark"
+    ];
+}
+
+var dot = document.querySelectorAll(".dot");
+
+// Init
+function checkCarousel() {
+    for (var i = 0; i < $(".themes .dot").length; i++) {
+        if (dot[i].classList.contains("active")) {
+            themes_section_title.textContent = themes_desc[i];
+            themes_section_hent.textContent = themes_hent[i];
+        }
+    }
+}
+
 $(document).ready(function() {
 
+    checkCarousel();
+
     // Change carousel indecators active
-    $(".dot").on("click", function() {
+    $(".themes .dot").on("click", function() {
         $(".dot").removeClass("active");
         $(this).addClass("active");
+        checkCarousel();
     });
 
-    // change carousel slide by swip
-    // $('.carousel').on('touchstart', function(event){
-    //     const xClick = event.originalEvent.touches[0].pageX;
-    //     $(this).one('touchmove', function(event){
-    //         const xMove = event.originalEvent.touches[0].pageX;
-    //         const sensitivityInPx = 5;
-    
-    //         if( Math.floor(xClick - xMove) > sensitivityInPx ){
-    //             $(this).carousel('prev');
-    //         }
-    //         else if( Math.floor(xClick - xMove) < -sensitivityInPx ){
-    //             $(this).carousel('next');
-    //         }
-    //     });
-    //     $(this).on('touchend', function(){
-    //         $(this).off('touchmove');
-    //     });
-    // });
- });
+    $("#carousel-themes").on("slid", () => {
+        console.log("0");
+        checkCarousel();
+    })
+});
